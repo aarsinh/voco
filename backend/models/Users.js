4bf4efa737522368f9fb6ae45d14a39fd3c7ad51
User Name <aditya.pjois@gmail.com>
2026-02-17 23:13:28 +0530

    added ngo schema

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const volunteerSchema = new mongoose.Schema({
     username: {
@@ -23,12 +24,45 @@ const volunteerSchema = new mongoose.Schema({
         type: String,
         match: [/^\d{10}$/, 'Please fill valid phone number']
     },
-    timestamps: true
-});
+}, { timestamps: true });
 
 const ngoSchema = new mongoose.Schema({
-
-});
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        validate: {
+            validator: function(v){
+                return validator.isEmail(v);
+            },
+            message: props => `${props.value} is not a valid email`
+        }
+    },
+    phoneNumber: {
+        type: String,
+        match: [/^\d{10}$/, 'Please fill valid phone number'],
+        required: true
+    },
+    website: {
+        type: String,
+        validate: {
+            validator: function(v){
+                return validator.isURL(v, { protocols: ['http', 'https'] });
+            },
+            message: `Invalid URL`
+        }
+    }
+}, { timestamps: true });
 
 module.exports = Volunteer = mongoose.model('volunteer', volunteerSchema);
 module.exports = NGO = mongoose.model('ngo', ngoSchema);
