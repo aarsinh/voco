17a6076f93dd5ef53143acf0b93da7e5a8fa214a
aarsinh <148481261+aarsinh@users.noreply.github.com>
2026-02-20 15:01:32 +0530

    Modified schema files

---
+++ backend/src/models/ngo.model.ts
@@ -0,0 +1,53 @@
+import mongoose from 'mongoose'
+import validator from 'validator'
+import bcrypt from 'bcrypt'
+
+const ngoSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    validate: {
+      validator: function(v: string) {
+        return validator.isEmail(v);
+      },
+      message: props => `${props.value} is not a valid email`
+    }
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  phoneNumber: {
+    type: String,
+    match: [/^\d{10}$/, 'Please fill valid phone number'],
+    required: true
+  },
+  website: {
+    type: String,
+    validate: {
+      validator: function(v: string) {
+        return validator.isURL(v, { protocols: ['http', 'https'] });
+      },
+      message: `Invalid URL`
+    }
+  }
+}, { timestamps: true });
+
+ngoSchema.pre("save", async function() {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10)
+  }
+})
+
+export const NGO = mongoose.model('ngo', ngoSchema);
---
+++ backend/src/models/volunteer.model.ts
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
+import validator from 'validator'
+
+const volunteerSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v: string) {
+        return validator.isEmail(v)
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    min: 18,
+    max: 100
+  },
+  sex: {
+    type: String,
+    enum: ['Male', 'Female', 'Other']
+  },
+  phoneNumber: {
+    type: String,
+    match: [/^\d{10}$/, 'Please fill valid phone number']
+  },
+}, { timestamps: true });
+
+volunteerSchema.pre("save", async function() {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10)
+  }
+})
+
+export const Volunteer = mongoose.model('volunteer', volunteerSchema);
