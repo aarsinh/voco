c482ed5a13dd2cc47014e8ccc9dd0ee3ce0f38c2
Vishisht_TB <tbvishisht@gmail.com>
2026-03-26 12:03:19 +0530

    project schema

--- frontend/src/components/ngo/volunteerList.tsx
+++ frontend/src/components/ngo/volunteerList.tsx
@@ -1,16 +1,12 @@
-import Header from "./Headers"
-import Events from "./Events"
-import type { EventType } from "./types"
+import Header from "./Headers";
+import { useParams } from "react-router-dom";
 
-interface OngoingEventsProps {
-  events: EventType[]
-  deleteEvent: (id: string) => void
-}
 
 function volunteerList() {
+    const {id} = useParams();
   return (
     <div className="bg-white max-w-5xl mx-auto shadow-[0_10px_40px_rgba(0,0,0,0.08)] mt-10 mb-8 min-h-75 border border-gray-100 p-8 rounded-xl overflow-auto">
-      <Header />
+      <Header title = "Registered Volunteers"/>
       
         
     </div>
